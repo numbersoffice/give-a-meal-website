f344fb627ddc8bb9eddfee55377c3c90d116eed3
Maximilian Benner <maxibenner@gmail.com>
2025-02-16 12:18:12 -0500

    fix: limited business name and item name in recent items marquee

--- src/sections/CTA/RecentItem/index.tsx
+++ src/sections/CTA/RecentItem/index.tsx
@@ -1,52 +1,103 @@
 import styles from "./styles.module.css";
 import Image from "next/image";
-import NewDonation from "@/public/assets/icons/newDonation.svg"
-import NewBusiness from "@/public/assets/icons/newBusiness.svg"
+import NewDonation from "@/public/assets/icons/newDonation.svg";
+import NewBusiness from "@/public/assets/icons/newBusiness.svg";
 import { Locale } from "@/i18n-config";
 
-export default function RecentItem({ lang, action, businessName, time, donorName, item }: { lang: Locale, action: "newDonation" | "newBusiness", businessName: string, time: string, donorName: string, item: string }) {
+export default function RecentItem({
+  lang,
+  action,
+  businessName,
+  time,
+  donorName,
+  item,
+}: {
+  lang: Locale;
+  action: "newDonation" | "newBusiness";
+  businessName: string;
+  time: string;
+  donorName: string;
+  item: string;
+}) {
+  let icon = action === "newDonation" ? NewDonation : NewBusiness;
+  let message;
 
-    let icon = action === "newDonation" ? NewDonation : NewBusiness
-    let message;
+  // Truncate item and business name if they are too long
+  if (item.length > 30) item = item.slice(0, 30) + "...";
+  if (businessName.length > 20)
+    businessName = businessName.slice(0, 20) + "...";
 
-
-    if (lang === "es") {
-        // Spanish
-        if (action === "newDonation") {
-            if (donorName) {
-                "Spindrift donado en Livite por Abby."
-                message = (<>{item} donado en <span className="body_l_bold">{businessName}</span> por <span className="body_l_bold">{donorName}</span></>)
-            } else {
-                message = (<>A {item} has been donated at <span className="body_l_bold">{businessName}</span></>)
-            }
-
-        } else {
-            message = (<><span className="body_l_bold">{businessName}</span> ahora es socio de restaurante</>)
-        }
+  if (lang === "es") {
+    // Spanish
+    if (action === "newDonation") {
+      if (donorName) {
+        ("Spindrift donado en Livite por Abby.");
+        message = (
+          <>
+            {item} donado en <span className="body_l_bold">{businessName}</span>{" "}
+            por <span className="body_l_bold">{donorName}</span>
+          </>
+        );
+      } else {
+        message = (
+          <>
+            A {item} has been donated at{" "}
+            <span className="body_l_bold">{businessName}</span>
+          </>
+        );
+      }
     } else {
-        // English
-        if (action === "newDonation") {
-            if (donorName) {
-                message = (<><span className="body_l_bold">{donorName}</span> donated a {item} at <span className="body_l_bold">{businessName}</span></>)
-            } else {
-                message = (<>A {item} has been donated at <span className="body_l_bold">{businessName}</span></>)
-            }
-
-        } else {
-            message = (<><span className="body_l_bold">{businessName}</span> became a restaurant partner</>)
-        }
+      message = (
+        <>
+          <span className="body_l_bold">{businessName}</span> ahora es socio de
+          restaurante
+        </>
+      );
     }
+  } else {
+    // English
+    if (action === "newDonation") {
+      if (donorName) {
+        message = (
+          <>
+            <span className="body_l_bold">{donorName}</span> donated a {item} at{" "}
+            <span className="body_l_bold">{businessName}</span>
+          </>
+        );
+      } else {
+        message = (
+          <>
+            A {item} has been donated at{" "}
+            <span className="body_l_bold">{businessName}</span>
+          </>
+        );
+      }
+    } else {
+      message = (
+        <>
+          <span className="body_l_bold">{businessName}</span> became a
+          restaurant partner
+        </>
+      );
+    }
+  }
 
-
-    return (
-        <div className={styles.container}>
-            <div className={styles.imageContainer}>
-                <Image priority className={styles.image} src={icon} alt="Donation or restaurant icon" width={36} height={36} />
-            </div>
-            <div className={styles.textContainer}>
-                <p className="body_l">{message}</p>
-                <p className="body_s">{time}</p>
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        <Image
+          priority
+          className={styles.image}
+          src={icon}
+          alt="Donation or restaurant icon"
+          width={36}
+          height={36}
+        />
+      </div>
+      <div className={styles.textContainer}>
+        <p className="body_l">{message}</p>
+        <p className="body_s">{time}</p>
+      </div>
+    </div>
+  );
 }
--- src/sections/CTA/index.tsx
+++ src/sections/CTA/index.tsx
@@ -1,11 +1,11 @@
-import Button from "@/components/button"
-import styles from "./styles.module.css"
-import RecentItem from "./RecentItem"
+import Button from "@/components/button";
+import styles from "./styles.module.css";
+import RecentItem from "./RecentItem";
 import Marquee from "react-fast-marquee";
 import Image from "next/image";
-import Tomato from "@/public/assets/tomato.svg"
-import Cucumber from "@/public/assets/cucumber.svg"
-import Onion from "@/public/assets/onion.svg"
+import Tomato from "@/public/assets/tomato.svg";
+import Cucumber from "@/public/assets/cucumber.svg";
+import Onion from "@/public/assets/onion.svg";
 import { Locale } from "@/i18n-config";
 import { getDictionary } from "@/get-dictionary-server";
 import localeLink from "@/utils/localeLink";
@@ -13,42 +13,62 @@ import getRecentDonationsAndBusinesses from "@/functions/getRecentDonationsAndBu
 
 /**
  * CTA section displaying three buttons and a marquee of recent donations
- * @param lang Sets the language of the component 
+ * @param lang Sets the language of the component
  */
 export default async function CTA({ lang }: { lang: Locale }) {
-    const dictionary = await getDictionary(lang)
-    const data = await getRecentDonationsAndBusinesses(dictionary)
+  const dictionary = await getDictionary(lang);
+  const data = await getRecentDonationsAndBusinesses(dictionary);
 
-    if (!dictionary) return null
-    return (
-        <div className="grid">
-            <div className={styles.container}>
-                <div className={styles.textContainer}>
-                    <h2>{dictionary.pages.home.cta.title}</h2>
-                    <div className={styles.buttonContainer}>
-                        <Button className={styles.button} href={localeLink("/get-a-meal", lang)}>{dictionary.pages.home.cta.button_1}</Button>
-                        <Button className={styles.button} href={localeLink("/give-a-meal", lang)}>{dictionary.pages.home.cta.button_2}</Button>
-                        <Button className={styles.button} href={localeLink("/restaurants", lang)}>{dictionary.pages.home.cta.button_3}</Button>
-                    </div>
-                </div>
-                {data && <div className={styles.recentItemsContainer}>
-                    <Marquee>
-                        {data.map((data: any) => <RecentItem lang={lang} key={data.id} action={data.action} businessName={data.businessName} time={data.time} donorName={data.donorName} item={data.item} />)}
-                    </Marquee>
-                </div>}
-            </div>
-            <div className={styles.vegContainer}>
-                <Image src={Tomato} alt="tomato" width={287} height={420} />
-                <Image src={Cucumber} alt="cucumber" width={377} height={430} />
-                <Image src={Onion} alt="onion" width={285} height={420} />
-            </div>
+  if (!dictionary) return null;
+  return (
+    <div className="grid">
+      <div className={styles.container}>
+        <div className={styles.textContainer}>
+          <h2>{dictionary.pages.home.cta.title}</h2>
+          <div className={styles.buttonContainer}>
+            <Button
+              className={styles.button}
+              href={localeLink("/get-a-meal", lang)}
+            >
+              {dictionary.pages.home.cta.button_1}
+            </Button>
+            <Button
+              className={styles.button}
+              href={localeLink("/give-a-meal", lang)}
+            >
+              {dictionary.pages.home.cta.button_2}
+            </Button>
+            <Button
+              className={styles.button}
+              href={localeLink("/restaurants", lang)}
+            >
+              {dictionary.pages.home.cta.button_3}
+            </Button>
+          </div>
         </div>
-    )
-
+        {data && (
+          <div className={styles.recentItemsContainer}>
+            <Marquee>
+              {data.map((data: any) => (
+                <RecentItem
+                  lang={lang}
+                  key={data.id}
+                  action={data.action}
+                  businessName={data.businessName}
+                  time={data.time}
+                  donorName={data.donorName}
+                  item={data.item}
+                />
+              ))}
+            </Marquee>
+          </div>
+        )}
+      </div>
+      <div className={styles.vegContainer}>
+        <Image src={Tomato} alt="tomato" width={287} height={420} />
+        <Image src={Cucumber} alt="cucumber" width={377} height={430} />
+        <Image src={Onion} alt="onion" width={285} height={420} />
+      </div>
+    </div>
+  );
 }
-
-
-
-
-
-
